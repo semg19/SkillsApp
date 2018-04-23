8c85f69cb70ad1be777afde930cba48ad0675b5e
semg19 <0913247@hr.nl>
2018-04-23 19:04:41 +0200

    first commit (bug)

---
+++ app/src/main/java/skills/com/sem/skillsapp/LoginActivity.java
@@ -0,0 +1,99 @@
+package skills.com.sem.skillsapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmailText;
+    private EditText loginPassText;
+    private Button loginBtn;
+    private Button loginRegBtn;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressBar loginProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginEmailText = (EditText) findViewById(R.id.login_email);
+        loginPassText = (EditText) findViewById(R.id.login_password);
+        loginBtn = (Button) findViewById(R.id.login_btn);
+        loginRegBtn = (Button) findViewById(R.id.login_reg_btn);
+        loginProgress = (ProgressBar) findViewById(R.id.login_progress);
+
+        loginRegBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent regintent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(regintent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String loginEmail = loginEmailText.getText().toString();
+                String loginPass = loginPassText.getText().toString();
+
+                if (!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPass)) {
+                    loginProgress.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+
+                                sendToMain();
+
+                            } else {
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this,"Error : " + errorMessage, Toast.LENGTH_LONG).show();
+                            }
+
+                            loginProgress.setVisibility(View.INVISIBLE);
+
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            sendToMain();
+        }
+    }
+
+    private void sendToMain() {
+        Intent mainintent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainintent);
+        finish();
+    }
+}
