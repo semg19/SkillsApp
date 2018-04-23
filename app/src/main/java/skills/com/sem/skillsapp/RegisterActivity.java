6bc525bd53e0f172fbf4d75fb356d34e0fd7f448
semg19 <0913247@hr.nl>
2018-04-23 20:53:50 +0200

    Bug fixes & login, register and account added

--- app/src/main/java/skills/com/sem/skillsapp/RegisterActivity.java
+++ app/src/main/java/skills/com/sem/skillsapp/RegisterActivity.java
@@ -3,10 +3,7 @@ package skills.com.sem.skillsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -55,44 +52,44 @@ public class RegisterActivity extends AppCompatActivity {
         reg_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    String email = reg_email_field.getText().toString();
-                    String pass = reg_pass_field.getText().toString();
-                    String confirm_pass = reg_confirm_pass_field.getText().toString();
+                String email = reg_email_field.getText().toString();
+                String pass = reg_pass_field.getText().toString();
+                String confirm_pass = reg_confirm_pass_field.getText().toString();
 
-                    if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)&& !TextUtils.isEmpty(confirm_pass)) {
-                        if (pass.equals(confirm_pass)){
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)&& !TextUtils.isEmpty(confirm_pass)) {
+                    if (pass.equals(confirm_pass)){
 
-                            mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
 
-                                    if (task.isSuccessful()){
+                                if (task.isSuccessful()){
 
-                                        Intent setupIntent = new Intent(RegisterActivity.this, SetupActivity.class);
-                                        startActivity(setupIntent);
-                                        finish();
+                                    Intent setupIntent = new Intent(RegisterActivity.this, SetupActivity.class);
+                                    startActivity(setupIntent);
+                                    finish();
 
-                                    } else {
+                                } else {
 
-                                        String errorMessage = task.getException().getMessage();
-                                        Toast.makeText(RegisterActivity.this, "Error: " + errorMessage, Toast.LENGTH_LONG).show();
+                                    String errorMessage = task.getException().getMessage();
+                                    Toast.makeText(RegisterActivity.this, "Error: " + errorMessage, Toast.LENGTH_LONG).show();
 
-
-                                    }
-
-                                    reg_progress.setVisibility(View.VISIBLE);
 
                                 }
-                            });
 
-                        } else {
+                                reg_progress.setVisibility(View.VISIBLE);
 
-                            Toast.makeText(RegisterActivity.this, "Password and Confirm Pasword field doesn't match.", Toast.LENGTH_LONG).show();
+                            }
+                        });
 
-                        }
+                    } else {
+
+                        Toast.makeText(RegisterActivity.this, "Password and Confirm Pasword field doesn't match.", Toast.LENGTH_LONG).show();
+
                     }
-
                 }
+
+            }
         });
     }
 
