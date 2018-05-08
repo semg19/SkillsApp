707f0f3e45ef4d5aad69db1b65e653ddf22f16e4
semg19 <0913247@hr.nl>
2018-05-08 16:31:43 +0200

    Posting a skill (movie) to database

--- app/src/main/java/skills/com/sem/skillsapp/NewSkillActivity.java
+++ app/src/main/java/skills/com/sem/skillsapp/NewSkillActivity.java
@@ -1,20 +1,225 @@
 package skills.com.sem.skillsapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewSkillActivity extends AppCompatActivity {
 
     private Toolbar newSkillToolbar;
+    private ProgressBar newSkillProgress;
+
+    private EditText newPostDesc;
+    private Button newPostBtn;
+
+    private Uri videoUri = null;
+    private static final int REQUEST_CODE = 101;
+//    private StorageReference videoRef;
+    private StorageReference storageReference;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+
+    private String current_user_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_skill);
 
+//        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+//        final StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+//        videoRef = storageRef.child("/videos/" + uid + "/userIntro.3gp");
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        current_user_id = firebaseAuth.getCurrentUser().getUid();
+
         newSkillToolbar = findViewById(R.id.new_skill_toolbar);
         setSupportActionBar(newSkillToolbar);
         getSupportActionBar().setTitle("Add New Skill");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        newPostDesc = findViewById(R.id.new_skill_desc);
+        newPostBtn = findViewById(R.id.post_btn);
+        newSkillProgress = findViewById(R.id.new_skill_progress);
+
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String desc = newPostDesc.getText().toString();
+
+                if (!TextUtils.isEmpty(desc) && videoUri != null){
+
+                    newSkillProgress.setVisibility(View.VISIBLE);
+
+                    String randomName = FieldValue.serverTimestamp().toString();
+
+                    StorageReference filePath = storageReference.child("post_movies").child(randomName + ".3gp");
+                    filePath.putFile(videoUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                            if(task.isSuccessful()){
+
+                                String downloadUri = task.getResult().getDownloadUrl().toString();
+
+                                Map<String, Object> postMap = new HashMap<>();
+                                postMap.put("movie_url", downloadUri);
+                                postMap.put("desc", desc);
+                                postMap.put("user_id", current_user_id);
+                                postMap.put("timestamp", FieldValue.serverTimestamp());
+
+                                firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentReference> task) {
+
+                                        if (task.isSuccessful()) {
+
+                                            Toast.makeText(NewSkillActivity.this, "Upload completed", Toast.LENGTH_LONG).show();
+                                            Intent mainIntent = new Intent(NewSkillActivity.this, MainActivity.class);
+                                            startActivity(mainIntent);
+                                            finish();
+
+                                        } else {
+
+                                            String error = task.getException().getMessage();
+                                            Toast.makeText(NewSkillActivity.this, "Upload failed: " + error, Toast.LENGTH_LONG).show();
+
+                                        }
+
+                                        newSkillProgress.setVisibility(View.INVISIBLE);
+
+                                    }
+                                });
+
+                            } else {
+
+                                String error = task.getException().getMessage();
+                                Toast.makeText(NewSkillActivity.this, "Upload failed: " + error, Toast.LENGTH_LONG).show();
+                                newSkillProgress.setVisibility(View.INVISIBLE);
+
+                            }
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+    }
+
+//    public void upload(View view) {
+//        if (videoUri != null) {
+//            UploadTask uploadTask = videoRef.putFile(videoUri);
+//
+//            uploadTask.addOnFailureListener(new OnFailureListener() {
+//                @Override
+//                public void onFailure(@NonNull Exception e) {
+//                    Toast.makeText(NewSkillActivity.this, "Upload failed: " + e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+//                }
+//            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                    Toast.makeText(NewSkillActivity.this, "Upload completed",Toast.LENGTH_LONG).show();
+//                }
+//            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+//                    updateProgress(taskSnapshot);
+//                }
+//            });
+//        } else {
+//            Toast.makeText(NewSkillActivity.this, "Nothing to upload",Toast.LENGTH_LONG).show();
+//        }
+//    }
+
+//    private void updateProgress(UploadTask.TaskSnapshot taskSnapshot) {
+//
+//        long fileSize = taskSnapshot.getTotalByteCount();
+//        long uploadBytes = taskSnapshot.getBytesTransferred();
+//        long progress = (100 * uploadBytes) / fileSize;
+//
+//        ProgressBar progressBar = (ProgressBar) findViewById(R.id.new_skill_progressbar);
+//        progressBar.setProgress((int) progress);
+//
+//    }
+
+    public void record(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    public void download(View view) {
+        try {
+            final File localFile = File.createTempFile("userIntro", "3gp");
+
+            storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(NewSkillActivity.this, "Download complete", Toast.LENGTH_LONG).show();
+
+                    final VideoView videoView = (VideoView) findViewById(R.id.videoView);
+                    videoView.setVideoURI(Uri.fromFile(localFile));
+                    videoView.start();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(NewSkillActivity.this, "Download failed" + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(NewSkillActivity.this, "Failed to create temp file" + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        videoUri = data.getData();
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(NewSkillActivity.this, "Video saved to:\n" + videoUri, Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(NewSkillActivity.this, "Video recording cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(NewSkillActivity.this, "Failed to record video", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
